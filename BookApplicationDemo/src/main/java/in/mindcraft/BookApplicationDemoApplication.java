a111a0e335d2d1f0aae63d6f196df9a3ad4ae090
Ami123456 <30860721+Ami123456@users.noreply.github.com>
2025-07-30 18:06:30 +0530

    update on 30-7-25

---
+++ BookApplicationDemo/src/main/java/in/mindcraft/BookApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package in.mindcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApplicationDemoApplication.class, args);
+	}
+
+}
